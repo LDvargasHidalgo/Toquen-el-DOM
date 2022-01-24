a3fd040ea13233876bd0c9fc6e3748cf9e83f185
iElectro <ejose.acc@gmail.com>
2022-01-24 16:40:41 -0500

    Piano teclado

--- static/js/piano.js
+++ static/js/piano.js
@@ -1,7 +1,31 @@
 let keys = document.querySelectorAll(".key")
+let body = document.body
 
-const keyboardRel = {
-    
+const keyRel = {
+    c3: "q",
+    "c-3": "2",
+    d3: "w",
+    "d-3": "3",
+    e3: "e",
+    f3: "r",
+    "f-3": "5",
+    g3: "t",
+    "g-3": "6",
+    a3: "y",
+    "a-3": "7",
+    b3: "u",
+    c4: "z",
+    "c-4": "s",
+    d4: "x",
+    "d-4": "d",
+    e4: "c",
+    f4: "v",
+    "f-4": "g",
+    g4: "b",
+    "g-4": "h",
+    a4: "n",
+    "a-4": "j",
+    b4: "m",
 }
 
 const loadSound = function (fuente) {
@@ -19,10 +43,28 @@ keys.forEach(key => {
     key.addEventListener("mousedown", ()=>{
         sound.play()
         console.log(key.id)
+
     })
     key.addEventListener("mouseup", ()=>{
         sound.pause()
         sound.load()
+
+    })
+
+    body.addEventListener("keydown", (e)=> {
+        if(e.key === keyRel[key.id]) {
+            sound.play()
+            key.classList[key.classList.length-1] !== "active" && key.classList.toggle("active")
+        }
+
+    })
+
+    body.addEventListener("keyup", (e)=> {
+        if(e.key === keyRel[key.id]) {
+            sound.pause()
+            sound.load()
+            key.classList.toggle("active")
+        }
     })
 }
 )
